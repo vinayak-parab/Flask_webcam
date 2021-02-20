39d5c86f42c49f8cd5adcee3c4cfcd74785879fa
Vinayak Parab <parabvinayak.235@gmail.com>
2021-02-20 16:20:53 +0100

    added the if main statement in the file

--- app.py
+++ app.py
@@ -30,6 +30,9 @@ def index():
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 
 
